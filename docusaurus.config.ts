ce5912817f1e58a70b2d7b6ef8a508ee4a8469d3
Joran Marcy <joran.marcy@gmail.com>
2026-03-07 19:36:43 +0100

    docs: update social card image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -55,7 +55,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/meds-social-card.png',
     colorMode: {
       respectPrefersColorScheme: true,
     },
